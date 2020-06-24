ff440e063d0defe60965ff40a84bb6182aadb772
Pierre Jambet <pierre.jambet@gmail.com>
2020-06-24 11:13:47 -0400

    Add first basic test and Rakefile
    Run tests with `rake test`

--- lib/til/core.rb
+++ lib/til/core.rb
@@ -1,5 +1,4 @@
 module Til
   module Core
-    p 'Til Gem Loaded'
   end
 end
---
+++ test/test_til.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require 'til-rb'
+
+describe Til do
+  it 'does not crash' do
+    Til::Core
+  end
+end
