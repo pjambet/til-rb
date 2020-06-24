b4e2533e4a94dabea0eea165d2f306291605d684
Pierre Jambet <pierre.jambet@gmail.com>
2020-06-24 11:58:54 -0400

    Add run method to Til::Core and first basic tests
    Didn't know Minitest had a Mock class, seems to get the job done

--- lib/til/core.rb
+++ lib/til/core.rb
@@ -1,4 +1,39 @@
 module Til
-  module Core
+  class Core
+
+    def self.run
+      # Exit if `fzf` is not available
+      # Optionally print a spinner
+      # Grab the list of existing categories
+      # Feed them into fzf with a new entry to let the user type a category
+      # Grab the category from fzf
+      # Handle the case where it's a new category (as in, figure out the new place in the README)
+      # Open $VISUAL or $EDITOR with a tempfile
+      # Read the file content
+      # Update the README.md with a link to the new entry
+      # Create the new file
+      # Create a new commit
+      # Output a link to the file and the link to edit it
+      til = new
+      til.run
+    end
+
+    def initialize(kernel = Kernel)
+      @kernel = kernel
+    end
+
+    def run
+      check_dependencies
+    end
+
+    private
+
+    def check_dependencies
+      result = @kernel.system('which fzf', out: '/dev/null', err: '/dev/null')
+      unless result
+        raise "fzf is required, you can install it on macOS with 'brew install fzf'"
+      end
+    end
+
   end
 end
--- test/test_til.rb
+++ test/test_til.rb
@@ -2,7 +2,16 @@ require 'minitest/autorun'
 require 'til-rb'
 
 describe Til do
-  it 'does not crash' do
-    Til::Core
+  it 'has a run method' do
+    Til::Core.run
+  end
+
+  it 'exits if fzf is not available' do
+    error = assert_raises RuntimeError do
+      kernel_mock = Minitest::Mock.new
+      kernel_mock.expect :system, false, ['which fzf', { out: '/dev/null', err: '/dev/null' }]
+      Til::Core.new(kernel_mock).run
+    end
+    assert_match(/fzf is required, you can install it on macOS with 'brew install fzf'/, error.message)
   end
 end
